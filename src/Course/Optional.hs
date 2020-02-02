bb365fa000e5fcfd2be287c78b1c0451297b6f6e
Eric Vincent <eric@efvincent.com>
2020-02-02 01:08:00 -0500

    worked through optional.hs

--- src/Course/Optional.hs
+++ src/Course/Optional.hs
@@ -27,8 +27,8 @@ mapOptional ::
   (a -> b)
   -> Optional a
   -> Optional b
-mapOptional =
-  error "todo: Course.Optional#mapOptional"
+mapOptional _ Empty -> Empty
+mapOptional f (Full n) -> Full (f n)
 
 -- | Bind the given function on the possible value.
 --
@@ -44,8 +44,8 @@ bindOptional ::
   (a -> Optional b)
   -> Optional a
   -> Optional b
-bindOptional =
-  error "todo: Course.Optional#bindOptional"
+bindOptional _ Empty = Empty
+bindOptional f (Full n) -> f n
 
 -- | Return the possible value if it exists; otherwise, the second argument.
 --
@@ -58,8 +58,9 @@ bindOptional =
   Optional a
   -> a
   -> a
-(??) =
-  error "todo: Course.Optional#(??)"
+(??) Empty v -> v
+(??) (Full n) _ -> n
+
 
 -- | Try the first optional for a value. If it has a value, use it; otherwise,
 -- use the second value.
@@ -79,8 +80,8 @@ bindOptional =
   Optional a
   -> Optional a
   -> Optional a
-(<+>) =
-  error "todo: Course.Optional#(<+>)"  
+(<+>) Empty v -> v
+(<+>) (Full v) _ -> Full v
 
 applyOptional :: Optional (a -> b) -> Optional a -> Optional b
 applyOptional f a = bindOptional (\f' -> mapOptional f' a) f
